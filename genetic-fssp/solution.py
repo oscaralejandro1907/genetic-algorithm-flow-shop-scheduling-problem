34837a58cc8094584c6ffe297b6c35d3cd796d21
oscar.alejandro <oscarale930719@gmail.com>
2023-12-26 15:19:17 -0600

    Create solution.py

---
+++ genetic-fssp/solution.py
@@ -0,0 +1,11 @@
+import queue
+
+
+class Solution:
+    def __init__(self, instance):
+        self.instance = instance    # problem instance
+        self.job_order = list()
+        self.makespan = 0
+
+    def calculate_makespan(self):
+        pass
