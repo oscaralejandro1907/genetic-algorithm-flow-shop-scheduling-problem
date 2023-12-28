91df15bce0421518463ba72fd13543b8fc6cbfb3
oscar.alejandro <oscarale930719@gmail.com>
2023-12-27 21:04:59 -0600

    Update instance.py

--- genetic-fssp/instance.py
+++ genetic-fssp/instance.py
@@ -1,13 +1,7 @@
-class Operation:
-    def __init__(self, mach_id, pt):
-        self.machineid = mach_id
-        self.processing_time = pt
-
-
 class Job:
     def __init__(self, jid):
         self.jobid = jid
-        self.Operations = list()  # List of operations of this job
+        self.processing_time_on_machine = dict()  # List of operations of this job
 
 
 class Instance:
@@ -15,7 +9,6 @@ class Instance:
     def __init__(self, filename):
         self.n_jobs = None
         self.n_machines = None
-        self.pt = []  # processing times
 
         self.Jobs = list()  # List of objects Job type
 
@@ -33,5 +26,4 @@ class Instance:
                 self.Jobs.append(new_job)
                 fields = line.split()
                 for m in range(0, len(fields), 2):
-                    op_in_machine = Operation(fields[m], fields[m + 1])
-                    self.Jobs[j].Operations.append(op_in_machine)
+                    self.Jobs[j].processing_time_on_machine[fields[m]] = fields[m + 1]
