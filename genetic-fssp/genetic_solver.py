fb493639c65cb4fc219e4a98c654b6a63159e5c6
oscar.alejandro <oscarale930719@gmail.com>
2023-12-27 21:05:01 -0600

    Update genetic_solver.py

--- genetic-fssp/genetic_solver.py
+++ genetic-fssp/genetic_solver.py
@@ -1,4 +1,6 @@
 from instance import Instance
+from solution import Solution
+
 import numpy as np
 
 
@@ -10,15 +12,16 @@ class Solver:
         self.prob_mutation = pm  # Probability of mutation
         self.stop_generation = stop  # stopping condition
 
-        self.populations = []
+        # Initialization. Randomly generate an initial population (set of solutions)
+        self.population = []
 
-        # Create populations. Each population will be created as a random permutation of jobs, with its ids.
-        # We will end with a list of populations which size will be the n_pop we defined.
+        # Create population. Each chromosome (solution) will be created as a random permutation of jobs, with its ids.
+        # We will end with a list of chromosomes (population) which size will be the n_pop we defined.
         for i in range(self.n_population):
             p = list(np.random.permutation(self.instance.n_jobs))
-            while p in self.populations:  # if the population exists generate another, to avoid repetition
+            while p in self.population:  # if the population exists generate another, to avoid repetition
                 p = list(np.random.permutation(self.instance.n_jobs))
-            self.populations.append(p)
+            self.population.append(p)
 
         for i in range(self.stop_generation):
             # Select the parents
@@ -27,8 +30,15 @@ class Solver:
 
     def selection(self):
         """Selection operator"""
-        # Calculate the makespans of all generated populations
+        # Calculate the makespans of all solutions (chromosomes) in the generated population
         pop_makespans = dict()
-        for pop in self.populations:
-            print(pop)
+        s = Solution(self.instance)
+        solution_example = [2, 1, 0]
+        s.calculate_makespan(solution_example)
+        # for chrom in self.population:
+        #     s = Solution(self.instance)
+        #     c=chrom
+        #     s.calculate_makespan(chrom)
+        #     x=1
+
 
